cd73cf47b24331af8ae75a3391d4d84847f3cd6e
Okikiola Ojikutu <okikiolaojikutue@gmail.com>
2023-08-18 23:07:35 -0600

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
 function App() {
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon=dallas&appid=31d258f6bef8802ed3e1251fad394332`
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
