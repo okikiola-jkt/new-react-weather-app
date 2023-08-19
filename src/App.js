cce3c25d3cae09541facb5a191f282d00bc8cf1e
Okikiola Ojikutu <okikiolaojikutue@gmail.com>
2023-08-19 01:00:03 -0600

    css and scripts

--- src/App.js
+++ src/App.js
@@ -3,10 +3,36 @@ import axios from "axios";
 
 function App() {
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon=dallas&appid=31d258f6bef8802ed3e1251fad394332`
+  // const url = `https://api.openweathermap.org/data/2.5/forecast?q=los%20angeles&units=imperial&cnt=3&appid=03052ad0cbc1cf2c60c3d79961c72fd3`
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>los-angeles</p>
+          </div>
+          <div className="temp">
+            <h1>30°F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p className="bold">34°c</p>
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">12%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold">12 MPH</p>
+            <p>Winds</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
