a9b0c37016391a15c27066547f8bac3cd1d2cac8
Okikiola Ojikutu <okikiolaojikutue@gmail.com>
2023-08-20 19:33:42 -0600

    New updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
 
-  // const url = `https://api.openweathermap.org/data/2.5/forecast?q=los%20angeles&units=imperial&cnt=3&appid=03052ad0cbc1cf2c60c3d79961c72fd3`
+  const apiKey = process.env.REACT_APP_API_KEY;
+  console.log(apiKey); 
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
+  
+  const searchLocation = (event) => {
+    if (event.key === 'Enter'){
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      })
+    }
+  }
+
+  
   return (
     <div className="app">
+      <div className="search">
+        <input 
+         value = {location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress = {searchLocation}
+        placeholder="Enter Location"
+        type="text" 
+        />
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
